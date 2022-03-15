f24739c6e48c64e913e0ca328e1cb9047070bd0d
SidneySilvaAraujo <sidneiaraujo219@gmail.com>
2022-03-15 01:11:56 -0300

    Mudanças nas classes

--- bytbank/src/bytbank/Conta.java
+++ bytbank/src/bytbank/Conta.java
@@ -5,5 +5,18 @@ public class Conta {
 	double saldo;
 	int agencia;
 	String titular;
+	//metodos;
+	public void deposita (double valor) {
+		saldo = saldo + valor;
+ 	}
 	
+	public boolean saca(double valor) {
+		if (this.saldo>= valor) {
+			this.saldo = this.saldo - valor;
+			return true;
+		} else {
+			return false;
+		}
+		
+	}
 }
--- bytbank/src/bytbank/CriaConta.java
+++ bytbank/src/bytbank/CriaConta.java
@@ -2,11 +2,12 @@ package bytbank;
 
 public class CriaConta {
 	public static void main(String[] args) {
-			Conta primeiraConta = new Conta();
-		primeiraConta.saldo = 200.0;
-		//definindo ooutra conta;
-		Conta segundaConta = new Conta();
-		segundaConta.saldo = 50.0;
-		System.out.println("A primeira conta tem: " + primeiraConta.saldo + " e, a segunda conta tem: " + segundaConta.saldo);
+		Conta contaDoPaulo = new Conta();
+		contaDoPaulo.saldo = 100.0;
+		contaDoPaulo.deposita(50);
+		System.out.println(contaDoPaulo.saldo); //imprime;
+		contaDoPaulo.saca(20); //chama outro método, e saca uma quantidade;
+		System.out.println(contaDoPaulo.saldo); //imprime;
+		
 	}
 }
