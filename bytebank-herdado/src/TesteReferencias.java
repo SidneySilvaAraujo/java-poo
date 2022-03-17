3859276bfc93f581f47661c0680b2e4c78df3af6
SidneySilvaAraujo <sidneiaraujo219@gmail.com>
2022-03-17 05:49:21 -0300

    introdução ao polimorfismo

---
+++ bytebank-herdado/src/TesteReferencias.java
@@ -0,0 +1,16 @@
+
+public class TesteReferencias {
+	public static void main(String[] args) {
+		Gerente g1 = new Gerente();
+		g1.setSalario(5000.0);
+		Funcionario f = new Funcionario();
+		f.setSalario(2000.0);
+		EditorVideo e = new EditorVideo();
+		e.setSalario(3000.0);
+		ControleBonificacao controle = new ControleBonificacao();
+		controle.registra(g1);
+		controle.registra(f);
+		controle.registra(e);
+		System.out.println(controle.getSoma());
+	}
+}
